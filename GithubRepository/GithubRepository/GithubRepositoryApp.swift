ad86753c3fe6912c521f9439fe7e61b62fe2e606
Muhammad Yousuf Saif <yousufsaif8@gmail.com>
2023-01-20 12:46:55 +0400

    - initial commit

---
+++ GithubRepository/GithubRepository/GithubRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepositoryApp.swift
+//  GithubRepository
+//
+//  Created by Muhammad Yousuf Saif on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
